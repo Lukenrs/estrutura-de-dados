d82f75332d47de33790a3004817ec4160d409a88
Lukenrs <lukenbr@gmail.com>
2022-01-14 22:01:30 -0300

    primeiro commit

---
+++ Desktop/Estrutura/script.js
@@ -0,0 +1,8 @@
+let nome = "Luiz"
+let numero = 10
+let ano = (2022-numero)
+console.log(ano+numero)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
